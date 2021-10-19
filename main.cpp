3839b7c09bf06970f76faffb038a5b14d46df5e3
xTheCoderx <justaguyoney@gmail.com>
2021-10-20 01:43:13 +1300

    Example 1.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
+void New();
 
 int main() {
-    int *p = (int*)malloc(5 * sizeof(int));
-    if (p == nullptr) {
-        std::cout << "Failed to allocate memory" << "\n";
-        return -1;
-    }
-
-    *p = 5;
-    std::cout << *p << "\n";
-    free(p);
-    p = nullptr;
-    free(p);
+//    int *p = (int*)malloc(5 * sizeof(int));
+//    if (p == nullptr) {
+//        std::cout << "Failed to allocate memory" << "\n";
+//        return -1;
+//    }
+//
+//    *p = 5;
+//    std::cout << *p << "\n";
+//    free(p);
+//    p = nullptr;
+//    free(p);
+    New();
     return 0;
 }
+
+// TODO: Look into why it's giving error
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "UnusedValue"
+void New() {
+    int *p = new int(5);
+    *p = 6;
+    std::cout << *p << std::endl;
+    delete p;
+    p = nullptr;
+}
+#pragma clang diagnostic pop
